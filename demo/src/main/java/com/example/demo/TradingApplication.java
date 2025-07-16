a0a7fef04accfcc5b966b3aa8a6ce4d2be8e63d5
Awtuh <motionplaylove@gmail.com>
2025-07-17 01:01:42 +0200

    j'ai enlevé route inutile

--- demo/src/main/java/com/example/demo/TradingApplication.java
+++ demo/src/main/java/com/example/demo/TradingApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.context.annotation.Bean;
@@ -22,10 +21,6 @@ public class TradingApplication {
 		return new RestTemplate();
 	}
 
-	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "zizi") String name) {
-		return String.format("caca %s!", name);
-	}
 	@CrossOrigin(origins = "http://localhost:4200")
 	@GetMapping("/pricing")
 	public String getPricing() {
